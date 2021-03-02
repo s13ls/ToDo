59c8e33599e3ec1c16cab7bac02c3acfc4fbd1f8
Геннадий Кузьмин <61311344+s13ls@users.noreply.github.com>
2021-03-02 18:36:57 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+Vue.component('task', {
+    props: ['data'],
+    methods: {
+        task_done() {
+            this.$emit('task_done');
+        }
+    },
+    template: `
+    <div class="task">
+    <div>
+        <h3 class="task__title">{{data.title}}</h3>
+        <p class="task__desc">{{data.desc}}</p>
+    </div>
+    <button @click="task_done()" class="task__done">Ready</button>
+    </div>
+    `
+})
+
+const vm = new Vue({
+    el: '#app',
+    data: {
+        new_task: {
+            title:'',
+            desc: ''
+        },
+        tasks: [
+
+        ]
+    },
+    methods: {
+        delete_task(id) {
+            this.tasks.splice(id, 1);
+        },
+        add_task() {
+            if(this.new_task.title!='') {
+                this.tasks.push({
+                    title: this.new_task.title,
+                    desc: this.new_task.desc
+                });
+                this.new_task.title='';
+                this.new_task.desc='';
+                }
+            }
+        }
+    
+})
+
